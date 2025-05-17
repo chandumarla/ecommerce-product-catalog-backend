926ccdd42a0ede5e98937e9be8758313f86073ce
Chandu Marla <chandumarla99@gmail.com>
2025-05-17 19:37:16 +0530

    added images

--- catalog/models.py
+++ catalog/models.py
@@ -8,6 +8,7 @@ class Product(models.Model):
     description = models.TextField(blank=True)
     price = models.FloatField()
     category = models.CharField(max_length=50)
+    images = models.ImageField(upload_to='products/', blank=True)
 
     def __str__(self):
         return self.name
