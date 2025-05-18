932d70f31189a0676c8bf301fe1050a087319d29
Chandu Marla <chandumarla99@gmail.com>
2025-05-18 14:01:41 +0530

    updated the secretkey hardcoded

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@n#+gfngh2gdusbkgk*g!v0+xwdl+=6x7c1cbq+pb+ei#d!=jq'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-@n#+gfngh2gdusbkgk*g!v0+xwdl+=6x7c1cbq+pb+ei#d!=jq')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
