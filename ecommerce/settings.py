fcc4d9c72d58fed10af93f5ee671a8c29c590bb9
Chandu Marla <chandumarla99@gmail.com>
2025-05-18 11:21:10 +0530

    implement  token authentication for API

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -40,9 +40,12 @@ INSTALLED_APPS = [
     'rest_framework',
     'catalog',
     'django_filters',
+     'corsheaders',
+     'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',  ###
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -134,3 +137,21 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Media files settings
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
+
+############  For CORS
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8001"
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:8001"
+]
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
